d15fb560873959eb8405923b6cca5f59105a9b24
Avinash Gupta <guptaavinash302@gmail.com>
2025-05-22 14:55:06 +0530

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EmailForm from './components/Email.jsx';
+import VulnerabilityScanner, { ScanResults } from './components/CySerch.jsx';
+import AuthorPage from './components/AuthorPage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<VulnerabilityScanner />} />
+        <Route path="/scan-results" element={<ScanResults />} />
+        <Route path="/full-scan" element={<EmailForm />} />
+        <Route path='/author' element={<AuthorPage />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
